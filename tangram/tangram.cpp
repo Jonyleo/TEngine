2838dc80cd6ecbd9a462d20dcfde59de099a9baf
giuliocamasso <giuliocamasso@gmail.com>
2022-12-01 11:10:59 +0000

    Minor changes: 1) threshold set to 1.0e-5 again 2) Minor change to challenge3. I added the testEquation method since in the pdf is asked to "Test problem 3 by passing random matrices to the method that will test the equation."

--- tangram/tangram.cpp
+++ tangram/tangram.cpp
@@ -1,3 +1,8 @@
+/*
+	Assignment 1: GROUP 1
+		93728	| João Miguel Ferreira Oliveira
+		105147	| Giulio Camasso
+*/
 #include <iostream>
 
 #include <tengine/test.hpp>
@@ -7,12 +12,9 @@
 #include <glm/ext/matrix_relational.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#define THRESHOLD (float)1.0e-4
-
-// random matrix values range: [0, 1.0]
+#define THRESHOLD (float)1.0e-5
 #define MIN_RAND 10
 #define MAX_RAND 11
-
 
 class MathChallenge {
 
@@ -36,34 +38,34 @@ public:
 		float sin = glm::sin(angle);
 		float cos = glm::cos(angle);
 		glm::mat3 squared = dual_matrix * dual_matrix; 
-		return glm::mat3(1.0f) // identity matrix
-			 + sin * dual_matrix 
-			 + (1 - cos) * squared;
+		
+		return glm::mat3(1.0f)
+			+ sin * dual_matrix 
+			+ (1 - cos) * squared;
 	}
 
-	static void printMatrix(glm::mat3 matrix) {
+	static void printMat3(glm::mat3 matrix) {
 		glm::mat3 transposed = glm::transpose(matrix);
 		for(int i = 0; i < 3; ++i)
-				std::cout << glm::to_string(transposed[i]) << std::endl;
+			std::cout << glm::to_string(transposed[i]) << std::endl;
 	}
 
 	static float randNumber(int min, int max) {
 		return (rand() % min) + (max - min - 1); 
 	}
 
-	static glm::vec3 randVector(int min, int max) {
+	static glm::vec3 randVec3(int min, int max) {
 		return glm::vec3(randNumber(min,max),
 						 randNumber(min,max),
 						 randNumber(min,max));
 	}
 
-	static glm::mat3 randMatrix(int min, int max) {
-		return glm::mat3(randVector(min,max),
-						 randVector(min,max),
-						 randVector(min,max));
+	static glm::mat3 randMat3(int min, int max) {
+		return glm::mat3(randVec3(min,max),
+						 randVec3(min,max),
+						 randVec3(min,max));
 	}
 	
-
 	static void challenge1(glm::vec3 view, glm::vec3 up, glm::mat3 expected) {
 		glm::mat3 frame = calcCoordinateFrame(view, up);
 
@@ -76,12 +78,10 @@ public:
 		std::cout << "w= " << glm::to_string(frame[1]) << std::endl;
 		std::cout << "u= " << glm::to_string(frame[2]) << std::endl;
 
-		if(glm::all(glm::equal(frame, expected, THRESHOLD))) {
-
+		if(glm::all(glm::equal(frame, expected, THRESHOLD)))
 			std::cout << "Correct" << std::endl;
-		} else {
+		else
 			std::cout << "Wrong" << std::endl;
-		}
 		std::cout << "----------------" << std::endl;
 	}
 
@@ -98,52 +98,61 @@ public:
 
 		std::cout << "new_point= " << glm::to_string(new_point) << std::endl;
 
-		if(glm::all(glm::equal(new_point, expected, THRESHOLD))) {
+		if(glm::all(glm::equal(new_point, expected, THRESHOLD)))
 			std::cout << "Correct" << std::endl;
-		} else {
+		else
 			std::cout << "Wrong" << std::endl;
-		}
+
 		std::cout << "----------------" << std::endl;
 	}
 
-	static void challenge3(){
-		// random matrices creation: A
+	static void testEquation(glm::mat3 A, glm::mat3 B){
 		
-
-		glm::mat3 A = randMatrix(10, 11);
-		std::cout << std::endl << "A:" << std::endl;
-		printMatrix(A);
-		float detA = glm::determinant(A);
-		std::cout << std::endl << "detA: " << detA << std::endl;
-
-		if(detA == 0)
-			return;
-
-		glm::mat3 B = randMatrix(10, 11);
-
-		std::cout << std::endl << "B:" << std::endl;
-		printMatrix(B);
-		float detB = glm::determinant(B);
-		std::cout << std::endl << "detB: " << detB << std::endl;
-		if (detB == 0)
-			return;
-
-		// (AB)^-1 = (B^-1) (A^-1)
 		glm::mat3 firstMember = glm::inverse(A * B);
 		std::cout << "\n(AB)^-1:" << std::endl;
-		printMatrix(firstMember);
+		printMat3(firstMember);
 
 		glm::mat3 secondMember = glm::inverse(B) * glm::inverse(A);
 		std::cout << "\n(B^-1) (A^-1):" << std::endl;
-		printMatrix(secondMember);
-		
-		if(glm::all(glm::equal(firstMember, secondMember, THRESHOLD))) {
+		printMat3(secondMember);
+
+		if (glm::all(glm::equal(firstMember, secondMember, THRESHOLD)))
 			std::cout << "Correct" << std::endl;
-		} else {
+		else
 			std::cout << "Wrong" << std::endl;
-		}
-		
+
 		std::cout << "----------------" << std::endl;
+		
+	}
+	
+	static void challenge3() {
+
+		// random matrix creation: A
+		glm::mat3 A = randMat3(10, 11);
+		// std::cout << std::endl << "A:" << std::endl;
+		// printMat3(A);
+		float detA = glm::determinant(A);
+		// std::cout << std::endl << "detA: " << detA << std::endl;
+
+		if(detA == 0) {
+			std::cout << std::endl << "detA = 0. Return...\n";
+			return;
+		}
+
+		// random matrix creation: B
+		glm::mat3 B = randMat3(10, 11);
+		// std::cout << std::endl << "B:" << std::endl;
+		// printMat3(B);
+		float detB = glm::determinant(B);
+		// std::cout << std::endl << "detB: " << detB << std::endl;
+
+		if (detB == 0) {
+			std::cout << std::endl << "detB = 0. Return...\n";
+			return;
+		}
+
+		// prove: (AB)^-1 = (B^-1) (A^-1)
+		testEquation(A, B);
 	}
 };
 
@@ -151,6 +160,7 @@ public:
 int main() {
     std::cout << "Coordinate Frame Challenge" << std::endl;
 	std::cout << "----------------" << std::endl;
+
 	MathChallenge::challenge1(glm::vec3(1.0f, 0.0f, 0.0f),
 						      glm::vec3(0.0f, 1.0f, 0.0f),
 							  {
@@ -158,7 +168,7 @@ int main() {
 								glm::vec3(0.000000, 0.000000, -1.000000),
 								glm::vec3(-0.000000, 1.000000, 0.000000)
 							  });
-							  
+					  
 	MathChallenge::challenge1(glm::vec3(1.0f, 0.0f, 0.0f),
 						      glm::vec3(0.0f, 0.0f, 1.0f),
 							  {
